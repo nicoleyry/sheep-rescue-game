bb411847b72611bfcf745330f33cbf281aa83ad3
Nicole <yoyotpe@hotmail.com>
2025-11-12 11:29:12 +0100

    Added mute feature

--- Assets/RW/Scripts/Managers/SoundManager.cs
+++ Assets/RW/Scripts/Managers/SoundManager.cs
@@ -1,25 +1,60 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SoundManager : MonoBehaviour
 {
 	public static SoundManager Instance;
 
+	[Header("Audio Clips")]
 	public AudioClip shootClip;
 	public AudioClip sheepHitClip;
 	public AudioClip sheepDroppedClip;
 
+	[Header("UI References")]
+    public Image muteButtonImage;
+    public Sprite muteIcon;
+    public Sprite unmuteIcon;
+
 	private Vector3 cameraPosition;
 
-    void Awake()
-    {
-        Instance = this;
-		cameraPosition = Camera.main.transform.position;
-    }
+    private static bool isMuted = false;
 
-	void Update()
+	void Awake()
 	{
+		Instance = this;
+		cameraPosition = Camera.main.transform.position;
 
+		if (isMuted) {
+			// Game is muted
+			AudioListener.volume = 0;
+			if (muteButtonImage != null) {
+				muteButtonImage.sprite = muteIcon;
+			}
+		} else {
+			// Game is not muted
+			AudioListener.volume = 1;
+			if (muteButtonImage != null) {
+				muteButtonImage.sprite = unmuteIcon;
+			}
+		}
 	}
+	
+    public void ToggleMute()
+    {
+        isMuted = !isMuted;
+
+        if (isMuted) {
+            AudioListener.volume = 0;
+            if (muteButtonImage != null) {
+                muteButtonImage.sprite = muteIcon;
+            }
+        } else {
+            AudioListener.volume = 1;
+            if (muteButtonImage != null) {
+                muteButtonImage.sprite = unmuteIcon;
+            }
+        }
+    }
 
 	private void PlaySound(AudioClip clip)
 	{
